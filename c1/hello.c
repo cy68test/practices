1e80027e65bddd24f4b6ed7859d81c185a62edfd
cy68 <cy68@live.com>
2017-04-01 06:09:13 +0000

    changes

--- c1/hello.c
+++ c1/hello.c
@@ -1,7 +1,11 @@
 #include <stdio.h>
+#define SIZE 100
+#define min(x, y) ( (x > y) ? y : x)
 
 int main()
 {
   printf("Hello World!\n");
+  printf("Size %d\n", SIZE);
+  printf("min %d\n", min(3, 5));
   return 0;
 }
